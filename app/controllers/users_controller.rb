bb7a829705298d2f3cae090116b568b31641838f
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-02-23 11:38:38 +0700

    auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
+
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -13,11 +15,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
+
     if @user.update(user_params)
       flash[:notice] = "Update Success"
       redirect_to @user
@@ -29,6 +31,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Hi #{@user.username}, welcome to Alpha Blog!"
       redirect_to articles_path
     else
@@ -39,5 +42,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
