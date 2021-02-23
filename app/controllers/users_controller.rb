d3e32a4f921b2d2fe47aa53db2386386766eee16
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-02-23 16:46:07 +0700

    middleware + delete user associated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
-
-    @articles = @user.articles.paginate(page: params[:page], per_page: 2)
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
@@ -15,7 +16,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
@@ -39,6 +39,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles succesfully deleted"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -46,5 +53,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 end
