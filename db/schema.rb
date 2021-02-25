d561073007610b56fdf20415d5bfaebda977d795
Muhammad Donny Devanda <devandadonny@gmail.com>
2021-02-25 12:20:57 +0700

    unit test + controller test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_132944) do
+ActiveRecord::Schema.define(version: 2021_02_25_034108) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2021_02_23_132944) do
     t.time "created_at"
     t.time "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
